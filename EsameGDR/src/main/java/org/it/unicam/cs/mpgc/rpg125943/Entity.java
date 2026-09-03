cf49a6381afabb6bc9e4da419c164f92db105f76
Noam Ceka <noam.ceka@studenti.unicam.it>
2026-09-03 03:49:47 +0200

    Implementazione meccanica del Dodge

--- EsameGDR/src/main/java/org/it/unicam/cs/mpgc/rpg125943/Entity.java
+++ EsameGDR/src/main/java/org/it/unicam/cs/mpgc/rpg125943/Entity.java
@@ -1,28 +1,53 @@
 package org.it.unicam.cs.mpgc.rpg125943;
 
+import java.util.Random;
+
 abstract public class Entity {
 
     private String name;
     protected int stamina;
     protected int attack;
     protected int defense;
+    protected int speed;
     protected int level;
     protected double exp;
     private boolean alive=true;
 
+    private static final double dodgePerSpeed=0.015;
+    private static final double dodgeRelativo=0.015;
+    private static final double dodgeCap=0.75;
+    private static final double dodgeMin=0.05;
+
+    private static final Random RANDOM = new Random();
 
 
-    protected Entity(String name, int stamina, int attack, int defense, int level, double exp, boolean alive) {
+
+    protected Entity(String name, int stamina, int attack, int defense, int speed , int level, double exp, boolean alive) {
         this.name = name;
         this.stamina = stamina;
         this.attack = attack;
         this.defense = defense;
+        this.speed = speed;
         this.level = level;
         this.exp = exp;
         this.alive = alive;
     }
 
+    public double dodgeChance(Entity attacker) {
+        double baseDodge = this.speed * dodgePerSpeed;
+        double modificatore = (this.speed - attacker.getSpeed()) * dodgeRelativo;
+        double totalDodge = baseDodge + modificatore;
+
+        return Math.max(dodgeMin, Math.min(dodgeCap, totalDodge));
+    }
+
     public void attack (Entity target) {
+        double evasione = target.dodgeChance(this);
+        if (RANDOM.nextDouble() < evasione){
+            System.out.println(target.getName() + " ha schivato l'attacco di " + this.getName() + "!");
+            return;
+        }
+
         int damage = this.attack - target.getDefense();
         if (damage > 0) {
             target.takeDamage(damage);
@@ -57,6 +82,10 @@ abstract public class Entity {
 
     public int getDefense() {
         return defense;
+    }
+
+    public int getSpeed() {
+        return speed;
     }
 
     public int getLevel() {
